6f20c9026aecfcdc5e529113ebb9d360861e2588
mhenrixon <mikael@mhenrixon.com>
2020-05-13 09:05:54 +0200

    Remove weird unfinished documentation

--- lib/echocat.rb
+++ lib/echocat.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-#
-# This module is intended to be used for outputting your
-#
-# @author Mikael Henriksson <mikael@mhenrixon.com>
-#
 module Echocat
   def self.run
     puts 'Hello World!'
