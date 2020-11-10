f04415d3479970257a08aec5170cd7d3d5180349
Dumitru Railean <raileandumitru@protonmail.com>
2020-11-10 11:44:28 +0200

    call main methods and configure load path

--- lib/echocat.rb
+++ lib/echocat.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
+lib = File.expand_path('..', __FILE__ )
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
+require 'dry-struct'
+require 'echocat/menu'
+require 'echocat/import'
 module Echocat
   def self.run
-    puts 'Hello World!'
+    ImportFromCsv.import_all_data
+    Menu.call
   end
 end
+
+Echocat.run
